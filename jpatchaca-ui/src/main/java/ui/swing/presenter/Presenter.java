340019b60a149fa24ba9a11f21639b2d55542a63
kalecser <kalecser@b862728e-1e09-11de-896d-b39e94f1936c>
2009-04-13 21:49:25 +0000

    Start task screen
    git-svn-id: http://jpatchaca.googlecode.com/svn/trunk@30 b862728e-1e09-11de-896d-b39e94f1936c

--- jpatchaca-ui/src/main/java/ui/swing/presenter/Presenter.java
+++ jpatchaca-ui/src/main/java/ui/swing/presenter/Presenter.java
@@ -1,22 +1,12 @@
 package ui.swing.presenter;
 
-import java.awt.BorderLayout;
 import java.awt.Window;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.lang.ref.WeakReference;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import javax.swing.BorderFactory;
-import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.KeyStroke;
-
-import ui.swing.utils.SwingUtils;
 
 public class Presenter {
 
@@ -29,55 +19,14 @@ public class Presenter {
 	
 	public JDialog showOkCancelDialog(final ActionPane pane, String title){
 		
-		final JDialog dialog = createOkCancelDialog(pane, title, mainScreen);
-		openedWindows.add(new WeakReference<Window>(dialog));
-		
-		new Thread(){
-			@Override
-			public void run() {
-				dialog.setVisible(true);
-			}
-		}.start();
-		
-		return dialog;
+		final JDialog dialog = new OkCancelDialog(pane, title, mainScreen);
+		return showDialog(dialog);
 	}
 
-	private static JDialog createOkCancelDialog(final ActionPane pane, String title, JFrame mainScreen) {
-		final JDialog dialog = new JDialog(mainScreen);
-		SwingUtils.makeLocationrelativeToParent(dialog, mainScreen);
-		dialog.setTitle(title);
+	public JDialog showDialog(final JDialog dialog) {
+		openedWindows.add(new WeakReference<Window>(dialog));
+		dialog.setVisible(true);	
 		
-		JPanel panel = pane.getPanel();
-		if (panel == null)
-			throw new IllegalStateException("panel must not be null");
-		
-		panel.setBorder(BorderFactory.createEmptyBorder());
-		
-		dialog.setLayout(new BorderLayout());
-		dialog.add(pane.getPanel(), BorderLayout.CENTER);
-		dialog.add(new OkCancelPanel(
-				new Runnable() {
-					@Override
-					public void run() {
-						pane.action().run();
-						dialog.setVisible(false);
-					}
-				},
-				new Runnable() {
-					@Override
-					public void run() {
-						dialog.setVisible(false);
-					}
-				}
-				), BorderLayout.SOUTH);
-		
-		
-		dialog.getRootPane().registerKeyboardAction(new ActionListener(){@Override
-			public void actionPerformed(ActionEvent e) {
-				dialog.setVisible(false);
-			}}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
-		
-		dialog.pack();
 		return dialog;
 	}
 
---
+++ jpatchaca-ui/src/main/java/ui/swing/presenter/UIAction.java
@@ -0,0 +1,6 @@
+package ui.swing.presenter;
+
+public interface UIAction {
+
+	public void run() throws ValidationException;
+}
