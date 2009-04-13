736e4fe5181571833fa5049c6f62a391c306561e
kalecser <kalecser@b862728e-1e09-11de-896d-b39e94f1936c>
2009-04-13 21:52:22 +0000

    Start task screen
    git-svn-id: http://jpatchaca.googlecode.com/svn/trunk@33 b862728e-1e09-11de-896d-b39e94f1936c

--- jpatchaca-ui/src/main/java/ui/swing/utils/SwingUtils.java
+++ jpatchaca-ui/src/main/java/ui/swing/utils/SwingUtils.java
@@ -2,19 +2,46 @@ package ui.swing.utils;
 
 import java.awt.Point;
 import java.awt.Window;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.Callable;
+
+import javax.swing.SwingUtilities;
+
+import sun.swing.SwingUtilities2;
 
 public class SwingUtils {
 
-	
-	public static void makeLocationrelativeToParent(Window window, Window owner) {
-		if (owner == null)
+	public static void makeLocationrelativeToParent(final Window window,
+			final Window owner) {
+		if (owner == null) {
 			return;
-		Point location = derivePoint(owner.getLocation(), 50, 50);
+		}
+		final Point location = derivePoint(owner.getLocation(), 50, 50);
 		window.setLocation(location);
 	}
 
-	private static Point derivePoint(Point basePoint, int x, int y) {
-		return new Point((int)basePoint.getX() + x, (int)basePoint.getY() + y);
+	private static Point derivePoint(final Point basePoint, final int x,
+			final int y) {
+		return new Point((int) basePoint.getX() + x, (int) basePoint.getY() + y);
 	}
-	
+
+	public static <T> T getOrCry(final Callable<T> callable) {
+		try {
+			return SwingUtilities2.submit(callable).get();
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void invokeAndWaitOrCry(final Runnable runnable) {
+		try {
+			SwingUtilities.invokeAndWait(runnable);
+		} catch (final InterruptedException e) {
+			throw new RuntimeException(e);
+		} catch (final InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
 }
