0acd0d6f1d87f2f160d4bd23513d03ae5f38d730
kalecser <kalecser@b862728e-1e09-11de-896d-b39e94f1936c>
2009-05-07 00:32:20 +0000

    10:Varios cliques na tray criam vários start task
    git-svn-id: http://jpatchaca.googlecode.com/svn/trunk@95 b862728e-1e09-11de-896d-b39e94f1936c

--- jpatchaca-ui/src/main/java/ui/swing/tasks/StartTaskController.java
+++ jpatchaca-ui/src/main/java/ui/swing/tasks/StartTaskController.java
@@ -1,11 +1,14 @@
 package ui.swing.tasks;
 
+import javax.swing.JDialog;
+
 import ui.swing.presenter.Presenter;
 
 public class StartTaskController {
 
 	private final StartTaskScreen startTaskScreen;
 	private final Presenter presenter;
+	private JDialog dialog;
 
 	public StartTaskController(final Presenter presenter,
 			final StartTaskScreen startTaskScreen) {
@@ -14,6 +17,11 @@ public class StartTaskController {
 	}
 
 	public void show() {
-		presenter.showOkCancelDialog(startTaskScreen, "Start task");
+		if (dialog != null) {
+			dialog.setVisible(false);
+			dialog.dispose();
+		}
+
+		dialog = presenter.showOkCancelDialog(startTaskScreen, "Start task");
 	}
 }
