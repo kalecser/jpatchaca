8faa62e64e57e7f3f94ade54f0fe2e4e47bfcc4b
kalecser <kalecser@b862728e-1e09-11de-896d-b39e94f1936c>
2009-05-17 22:15:15 +0000

    Fixing ListSignalInterface 1 of 2
    git-svn-id: http://jpatchaca.googlecode.com/svn/trunk@100 b862728e-1e09-11de-896d-b39e94f1936c

---
+++ commons-core/src/main/java/reactive/ListSignal2.java
@@ -0,0 +1,23 @@
+package reactive;
+
+import lang.Maybe;
+
+import org.reactive.Signal;
+
+public interface ListSignal2<T> {
+
+	public Signal<Integer> size();
+
+	public void add(T value);
+
+	public Signal<Maybe<T>> get(Signal<Integer> source);
+
+	public Maybe<T> currentGet(int i);
+
+	public void remove(int i);
+
+	public int currentSize();
+
+	public void remove(T value);
+
+}
