f39be46fcb96dea9154a75f434005244fd62c641
kalecser <kalecser@b862728e-1e09-11de-896d-b39e94f1936c>
2010-07-13 19:53:43 +0000

    Removing Warning
    git-svn-id: http://jpatchaca.googlecode.com/svn/trunk@140 b862728e-1e09-11de-896d-b39e94f1936c

--- jpatchaca-system/src/main/java/main/singleInstance/AlreadyRunningApplicationException.java
+++ jpatchaca-system/src/main/java/main/singleInstance/AlreadyRunningApplicationException.java
@@ -1,6 +1,7 @@
 package main.singleInstance;
 
 public class AlreadyRunningApplicationException extends Exception {
+	private static final long serialVersionUID = 1L;
 
 	public AlreadyRunningApplicationException() {
 		super("This program is already running.");
