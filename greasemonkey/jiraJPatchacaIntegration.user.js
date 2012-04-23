2373b112d6420c140992a680f36cef7c06de4e56
gtakeuchi <gabriel.takeuchi@objective.com.br>
2012-04-23 11:17:29 -0300

    Some refactoring

--- greasemonkey/jiraJPatchacaIntegration.user.js
+++ greasemonkey/jiraJPatchacaIntegration.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name           InterceptUpdate
+// @name           JiraPatchacaIntegration
 // @namespace      taksan
 // @description    Intercept issue update
 // @include        https://jira.objective.com.br/secure/TaskBoard.jspa*
@@ -7,13 +7,25 @@
 // @require			http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
 
-$("[class^='iKey']").after(function(){
-	issueKey = this.title.replace(/.*JIRA -/,"");
-	cp = $("<br/><input type='button' value='&gt;' title='Time tracker on "+issueKey+"'>").click(function(){
-		$.get("http://127.0.0.1:48625/startTask ["+issueKey+ "]");
+main();
+
+function main()
+{
+	addStartButtonsOnTaskBoardIssues();
+	interceptProgressToStartIssue();
+}
+
+function addStartButtonsOnTaskBoardIssues()
+{
+	$("[class^='iKey']").after(function(){
+		issueKey = this.title.replace(/.*JIRA -/,"");
+		cp = $("<br/><input type='button' value='&gt;' title='Time tracker on "+issueKey+"'>").click(function(){
+			$.get("http://127.0.0.1:48625/startTask ["+issueKey+ "]");
+		})
+		return cp;
 	})
-    return cp;
-})
+}
+
 function getAssignee(key)
 {
 	return	$("#issueDisp_"+key+" div[title^='Assignee']").text().trim()
@@ -24,17 +36,17 @@ function getCurrentUser()
 	return $('#header-details-user a[class="lnk"][href="/secure/ViewProfile.jspa"]').text().trim();
 }
 
-unsafeWindow.Issue.prototype.updatingStatus_old = unsafeWindow.Issue.prototype.updatingStatus;
+function interceptProgressToStartIssue() {
+	unsafeWindow.Issue.prototype.updatingStatus_old = unsafeWindow.Issue.prototype.updatingStatus;
 
-unsafeWindow.Issue.prototype.updatingStatus = function(A) {
-	if (A == 'gh.boards.inprog_orphan') {
-		var assignee = getAssignee(this.key);
-		if (getCurrentUser() == assignee) {
-//			alert("Issue " + this.key + " set in progress. Assignee: " + getAssignee(this.key) + '. Will start activity on time tracker');
+	unsafeWindow.Issue.prototype.updatingStatus = function(A) {
+		if (A == 'gh.boards.inprog_orphan') {
+			var assignee = getAssignee(this.key);
+			if (getCurrentUser() == assignee) {
 
-			$.get("http://127.0.0.1:48625/startTask ["+this.key+ "]");
+				$.get("http://127.0.0.1:48625/startTask ["+this.key+ "]");
+			}
 		}
+		this.updatingStatus_old(A);
 	}
-	this.updatingStatus_old(A);
 }
-
