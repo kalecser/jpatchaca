ea6fd76ee0dbf234f16d1973b8f42df411034f9d
takeuchi <g.takeuchi@gmail.com>
2012-04-23 20:51:29 -0300

    Uses a common method to add the button to a shared toolbar

--- greasemonkey/greenhopper_integration.user.js
+++ greasemonkey/greenhopper_integration.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name           InterceptUpdate
+// @name           JiraJPatchacaIntegration
 // @namespace      taksan
 // @description    Intercept issue update
 // @include        https://jira.objective.com.br/secure/TaskBoard.jspa*
--- greasemonkey/jira_integration.user.js
+++ greasemonkey/jira_integration.user.js
@@ -4,6 +4,7 @@
 // @description    Start Task
 // @include        https://jira.objective.com.br/browse/*
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// @require       https://raw.github.com/taksan/objective-monkey-utils/master/jiraToolbar.js
 // ==/UserScript==
 
 main();
@@ -11,8 +12,10 @@ main();
 function main (){
         issueId = $('[id^=issue_key]').text();
 
-        $('body').prepend(
+//        $('body').prepend(
+			addToToolbar(        
                 $('<input type="button" value="Start task" style="position:fixed;top:5px;left:5px;">').click(function() {
-			$.get("http://127.0.0.1:48625/startTask ["+issueId+ "]");
-        }))
+					$.get("http://127.0.0.1:48625/startTask ["+issueId+ "]")
+	        }))
+//        )
 }
