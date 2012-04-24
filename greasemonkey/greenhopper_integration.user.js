27d77af0a1b081771106a2784e8ad108d795884b
gtakeuchi <gabriel.takeuchi@objective.com.br>
2012-04-24 09:22:12 -0300

    Just some refactoring

--- greasemonkey/greenhopper_integration.user.js
+++ greasemonkey/greenhopper_integration.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name           JPatcharGreenhopperIntegration
+// @name           JPatchacaGreenhopperIntegration
 // @namespace      taksan
 // @description    Intercept issue update
 // @include        https://jira.objective.com.br/secure/TaskBoard.jspa*
@@ -7,19 +7,44 @@
 // @require			http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
 
-$("[class^='iKey']").after(function(){
-	var issueKey = this.title.replace(/.*JIRA -/,"");
-	var nl='<br/>';
-	if ($(this).parent().attr('class') != 'list-subheader'){
-		nl = '';
-	}
+main();
 
-	cp = $(nl+"<input type='button' value='&gt;' title='Time tracker on "+issueKey+"'>");
-	cp.click(function(){
-		$.get("http://127.0.0.1:48625/startTask ["+issueKey+ "]");
+function main()
+{
+	addStartButtonsToIssueCards();
+	interceptIssueTransitionToStartTask();
+}
+
+function addStartButtonsToIssueCards() {
+	$("[class^='iKey']").after(function(){
+		var issueKey = this.title.replace(/.*JIRA -/,"");
+		var nl='<br/>';
+		if ($(this).parent().attr('class') != 'list-subheader'){
+			nl = '';
+		}
+
+		cp = $(nl+"<input type='button' value='&gt;' title='Time tracker on "+issueKey+"'>");
+		cp.click(function(){
+			$.get("http://127.0.0.1:48625/startTask ["+issueKey+ "]");
+		})
+		return cp;
 	})
-    return cp;
-})
+}
+
+function interceptIssueTransitionToStartTask() {
+	unsafeWindow.Issue.prototype.updatingStatus_old = unsafeWindow.Issue.prototype.updatingStatus;
+
+	unsafeWindow.Issue.prototype.updatingStatus = function(A) {
+		if (A == 'gh.boards.inprog_orphan') {
+			var assignee = getAssignee(this.key);
+			if (getCurrentUser() == assignee) {
+				$.get("http://127.0.0.1:48625/startTask ["+this.key+"]");
+			}
+		}
+		this.updatingStatus_old(A);
+	}
+}
+
 function getAssignee(key)
 {
 	return	$("#issueDisp_"+key+" div[title^='Assignee']").text().trim()
@@ -30,14 +55,3 @@ function getCurrentUser()
 	return $('#header-details-user a[class="lnk"][href="/secure/ViewProfile.jspa"]').text().trim();
 }
 
-unsafeWindow.Issue.prototype.updatingStatus_old = unsafeWindow.Issue.prototype.updatingStatus;
-
-unsafeWindow.Issue.prototype.updatingStatus = function(A) {
-	if (A == 'gh.boards.inprog_orphan') {
-		var assignee = getAssignee(this.key);
-		if (getCurrentUser() == assignee) {
-			$.get("http://127.0.0.1:48625/startTask ["+this.key+"]");
-		}
-	}
-	this.updatingStatus_old(A);
-}
